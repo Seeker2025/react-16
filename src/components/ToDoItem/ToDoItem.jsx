3ccbaa324965dc98d7b216249922e312c26362fd
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-07-25 12:03:38 +0300

    handleCheck
    Added handleCheck function

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,11 +1,24 @@
 import css from './ToDoItem.module.css';
 
-export const ToDoItem = ({todoItem, toDelete}) =>{
+export const ToDoItem = ({
+    todoItem,
+    toDelete,
+    handle
+                        }) =>{
     return (
         <>
             <div className={css.line}>
                 <h3>{todoItem.title}</h3> 
                 <p>{`${todoItem.completed}`}</p>
+
+                <div>
+                    <input
+                    type="checkbox"
+                    checked={todoItem.completed}
+                    onChange={()=>handle(todoItem.id)}
+                    />
+
+                </div>
 
                 <button
                 type = "button"
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -16,24 +16,41 @@ export class ToDoList extends Component{
     }))
     }
 
+    handleCheck = (id) => {
+        this.setState((prevState)=>{
+            return {
+                todoList: prevState.todoList.map((todo)=>{
+                    console.log(todo);
+                return todo.id === id
+                ?{...todo, completed: !todo.completed}
+                :todo    
+                    
+                })
+            }
+        })
+    }
+
+    
+
     render(){
         return(
             <div>
                 <h1>My To Do List</h1>
                     <ul className={css.parLine}>
 
-                {
-                    this.state.toDoList.map(itm=>{
-                        return <li key ={nanoid()}>
-                            
-                                <ToDoItem
-                                todoItem = {itm}
-                                toDelete = {this.toDelete}
-                                />
-                                                        
-                                </li>
-                    })
-                }
+            {
+                this.state.toDoList.map(itm=>{
+                    return <li key ={nanoid()}>
+                        
+                            <ToDoItem
+                            todoItem = {itm}
+                            toDelete = {this.toDelete}
+                            handle={this.handleCheck}
+                            />
+                                                
+                            </li>
+                })
+            }
 
                     </ul>
 
